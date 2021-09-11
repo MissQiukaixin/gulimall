34c8d6f316ee69d250436cd4457bc6b8fe087898
邱凯芯 <501411356@qq.com>
2021-09-11 17:27:51 +0800

    qiukaixin

---
+++ gulimall-coupon/src/main/java/qiu/kai/xin/gulimall/coupon/GulimallCouponApplication.java
@@ -0,0 +1,13 @@
+package qiu.kai.xin.gulimall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCouponApplication.class, args);
+    }
+
+}
