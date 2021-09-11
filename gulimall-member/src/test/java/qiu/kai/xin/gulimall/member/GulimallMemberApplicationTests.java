438373910ee816f4424c70a3ba5130bbfaa8d9e8
邱凯芯 <501411356@qq.com>
2021-09-11 17:38:13 +0800

    qiu

---
+++ gulimall-member/src/test/java/qiu/kai/xin/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package qiu.kai.xin.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
